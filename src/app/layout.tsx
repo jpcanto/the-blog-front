17a378b654cc377f61c37cdb33fbf6e7e1b6d947
jpcanto <canto.jplopes@gmail.com>
2025-05-15 18:06:17 -0300

    fix: move global components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "The blog",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+type RootLayoutProps = {
   children: React.ReactNode;
-}>) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header />
+          {children}
+          <footer>
+            <p className="text-6xl font-bold text-center py-8">Footer</p>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { PostsList } from "@/components/Post/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
-import { Container } from "@/components/Container";
-import { Header } from "@/components/Header";
 import { PostFeatured } from "@/components/Post/PostFeatured";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -17,10 +13,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className="text-center font-bold text-6xl py-8">Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
