63be48e7bdae87d31797960526058eeb4bde2887
jpcanto <canto.jplopes@gmail.com>
2025-05-15 14:50:53 -0300

    feat: add basic post style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,39 @@ import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import Link from "next/link";
+import Image from "next/image";
+import clsx from "clsx";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section
+        className={clsx("grid grid-cols-1 gap-8 mb-16 group", "sm:grid-cols-2")}
+      >
+        <Link href="/" className="w-full h-full overflow-hidden rounded-xl">
+          <Image
+            src="/images/image-0.jpg"
+            alt="The Blog"
+            width={1020}
+            height={680}
+            className="group-hover:scale-105 transition-transform duration-300"
+          />
+        </Link>
+        <div>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
