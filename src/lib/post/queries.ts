c1d861b5d44b8004fdc98c35041efc32df8aa0d8
jpcanto <canto.jplopes@gmail.com>
2025-05-17 23:00:22 -0300

    feat: add cached queries with tag to invalidate

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,16 +1,37 @@
 import { postRepository } from "@/repositories/post";
+import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
-import { cache } from "react";
 
-export const findAllPublishedPostsCached = cache(
-  async () => await postRepository.findAllPublished()
+export const findAllPublishedPostsCached = unstable_cache(
+  async () => await postRepository.findAllPublished(),
+  ["posts"],
+  {
+    revalidate: 60,
+    tags: ["posts"],
+  }
 );
 
-export const findPostBySlugCached = cache(async (slug: string) => {
-  const post = await postRepository.findBySlug(slug).catch(() => notFound());
-  return post;
-});
+export const findPostBySlugCached = (slug: string) =>
+  unstable_cache(
+    async (slug: string) => {
+      const post = await postRepository
+        .findBySlug(slug)
+        .catch(() => notFound());
+      return post;
+    },
+    ["posts", "slug"],
+    {
+      revalidate: 60,
+      tags: [`posts:${slug}`],
+    }
+  )(slug);
 
-export const findPostByIdCached = cache(async (id: string) =>
-  postRepository.findById(id)
-);
+export const findPostByIdCached = (id: string) =>
+  unstable_cache(
+    async (id: string) => postRepository.findById(id),
+    ["posts", "id"],
+    {
+      revalidate: 60,
+      tags: [`posts:${id}`],
+    }
+  )(id);
